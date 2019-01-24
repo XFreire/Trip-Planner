a4c40055f4d0974f1f1975db8fae96033cf73412
Alexandre Freire <xandrefreire@gmail.com>
2019-01-24 08:37:24 +0000

    Add Connection model and test

---
+++ TripPlanner/Core/Common/Domain/Connection.swift
@@ -0,0 +1,54 @@
+//
+//  Connection.swift
+//  Core
+//
+//  Created by Alexandre Freire on 24/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+import Foundation
+
+struct Connection: Decodable {
+    
+    // MARK: Properties
+    let from: Location
+    let to: Location
+    let price: Int
+    
+    // MARK: - Decodable
+    private enum ContainerCodingKeys: String, CodingKey {
+        case from, to, price, coordinates
+    }
+    
+    private enum CoordinatesContainerCodingKeys: String, CodingKey {
+        case from, to
+    }
+    
+    private enum CoordinatesCodingKeys: String, CodingKey {
+        case lat, long
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: ContainerCodingKeys.self)
+        let coordinatesContainer = try container.nestedContainer(keyedBy: CoordinatesContainerCodingKeys.self, forKey: .coordinates)
+        let fromCoordinatesContainer = try coordinatesContainer.nestedContainer(keyedBy: CoordinatesCodingKeys.self, forKey: .from)
+        let toCoordinatesContainer = try coordinatesContainer.nestedContainer(keyedBy: CoordinatesCodingKeys.self, forKey: .to)
+        
+        // From Location
+        let fromName = try container.decode(String.self, forKey: .from)
+        let fromLat = try fromCoordinatesContainer.decode(Double.self, forKey: .lat)
+        let fromLong = try fromCoordinatesContainer.decode(Double.self, forKey: .long)
+        
+        self.from = Location(name: fromName, latitude: fromLat, longitude: fromLong)
+        
+        // To Location
+        let toName = try container.decode(String.self, forKey: .to)
+        let toLat = try toCoordinatesContainer.decode(Double.self, forKey: .lat)
+        let toLong = try toCoordinatesContainer.decode(Double.self, forKey: .long)
+        
+        self.to = Location(name: toName, latitude: toLat, longitude: toLong)
+        
+        // Price
+        self.price = try container.decode(Int.self, forKey: .price)
+    }
+}
---
+++ TripPlanner/CoreTests/ConnectionTests.swift
@@ -0,0 +1,32 @@
+//
+//  ConnectionTests.swift
+//  CoreTests
+//
+//  Created by Alexandre Freire on 24/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+import XCTest
+@testable import Core
+class ConnectionTests: XCTestCase {
+
+    private class Dummy {}
+    
+    var connection: Connection!
+    var connectionList: [Connection]!
+    
+    override func setUp() {
+        var path = Bundle(for: Dummy.self).path(forResource: "connection", ofType: "json")!
+        var data = try! Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
+        let decoder = JSONDecoder()
+        connection = try! decoder.decode(Connection.self, from: data)
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testConnectionExistence() {
+        XCTAssertNotNil(connection)
+    }
+}
