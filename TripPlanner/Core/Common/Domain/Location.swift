2675917da375c862e72bd82d9c09bed720a097c4
Alexandre Freire <xandrefreire@gmail.com>
2019-01-24 08:11:36 +0000

    Add Location model and test

---
+++ TripPlanner/Core/Common/Domain/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  Core
+//
+//  Created by Alexandre Freire on 24/01/2019.
+//  Copyright © 2019 Alexandre Freire. All rights reserved.
+//
+
+import Foundation
+
+typealias City = String
+
+struct Location {
+    
+    // MARK: Properties
+    let name: City
+    let latitude: Double
+    let longitude: Double
+}
--- TripPlanner/CoreTests/LocationTests.swift
+++ TripPlanner/CoreTests/LocationTests.swift
@@ -1,5 +1,5 @@
 //
-//  CoreTests.swift
+//  LocationTests.swift
 //  CoreTests
 //
 //  Created by Alexandre Freire on 24/01/2019.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Core
 
-class CoreTests: XCTestCase {
+class LocationTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,17 +18,9 @@ class CoreTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testLocationExistence() {
+        let location = Location(name: "London", latitude: 51.5285582, longitude: -0.241681)
+        XCTAssertNotNil(location)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
